df67d2f94021010a0c8941e473e48db9eb9ed07f
Jay comendador <jcomendador120@gmail.com>
2026-09-09 14:41:47 -0700

    Initial MERN project

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "MERN backend is running!"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
